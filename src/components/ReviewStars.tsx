047acf530575264dbc4684fab2166cb4618092bd
Anthony Emberley <anthonyemberley@anthonys-mbp-2.lan>
2020-10-12 11:00:39 -0700

    5 stars component done

--- src/components/ReviewStars.tsx
+++ src/components/ReviewStars.tsx
@@ -8,20 +8,27 @@ type ReviewStarsProps = {
 
 export default function ReviewStars(props: ReviewStarsProps) {
   let styles: { [key: string]: CSSProperties } = {
-    text: { paddingLeft: 5 },
     container: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
+      display: "inline-block",
     },
   };
   return (
     <div style={styles.container}>
-      <img src={starFilled} style={{ width: 18, height: 18 }} alt={"star"} />
-      <img src={starFilled} style={{ width: 18, height: 18 }} alt={"star"} />
-      <img src={starFilled} style={{ width: 18, height: 18 }} alt={"star"} />
-      <img src={starFilled} style={{ width: 18, height: 18 }} alt={"star"} />
-      <img src={starEmpty} style={{ width: 18, height: 18 }} alt={"star"} />
+      {[...Array(props.numStars)].map((value: undefined, index: number) => (
+        <img
+          src={starFilled}
+          style={{ width: 18, height: 18 }}
+          key={index}
+          alt={"star filled"}
+        />
+      ))}
+      {[...Array(5 - props.numStars)].map((value: undefined, index: number) => (
+        <img
+          src={starEmpty}
+          style={{ width: 18, height: 18 }}
+          alt={"star empty"}
+        />
+      ))}
     </div>
   );
 }
